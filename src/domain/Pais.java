5b1ff50083b63198d8605945fd2bea007d789de4
unknown <valentin.ortizcancino@alu.frt.utn.edu.ar>
2025-04-14 15:25:44 -0300

    Crear rama alta-animal

---
+++ src/domain/Pais.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+public class Pais {
+    private final String nombre;
+    private final String codigoIso;
+
+    public Pais(String nombre, String codigoIso) {
+        this.nombre = nombre;
+        this.codigoIso = codigoIso;
+
+    }
+}
